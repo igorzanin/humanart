6f6d0d8db1d732e6565bb6ac5a759aef9b2e3f1e
Igor <igor.zanin@human.art.br>
2023-06-26 23:27:25 -0300

    Testing colors schemes;

--- source/sidemenu.py
+++ source/sidemenu.py
@@ -8,14 +8,14 @@ class SideMenu(ft.UserControl):
         self.page = page
         self.visible = True
         # self.theme: ft.Theme = page.theme
+        self.color_scheme = theme.AppTheme(light_mode=True)
         self.sidebar = ft.Container(
                                      width=200,
                                      padding=0,
                                      margin=0,
-                                     bgcolor=ft.colors.SECONDARY_CONTAINER
+                                     bgcolor=self.color_scheme.secondary_container
                                     )
 
-        print(self.theme.color_scheme.secondary_container)
         self.page.on_route_change = self.set_styles
 
     def set_styles(self):
